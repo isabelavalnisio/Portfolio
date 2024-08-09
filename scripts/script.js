cb8030ec6f29db1acbe905a7fcf69bed720fff97
Luís <luisgmsferreira@gmail.com>
2024-08-09 14:54:42 +0100

    make the overlay on top more responsive after

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,3 @@
-// Add any JavaScript functionality here if needed
-
-// Example of a simple function to toggle a class
 document.addEventListener("DOMContentLoaded", function () {
     const themeCheckbox = document.querySelector('.theme .input');
     const body = document.body;
